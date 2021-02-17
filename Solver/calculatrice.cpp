bca2621bf29685e30d03d731adfb1834bd157743
Matthias <matthias.launay@hotmail.com>
2021-02-17 13:44:07 +0100

    variable v2

--- Solver/calculatrice.cpp
+++ Solver/calculatrice.cpp
@@ -10,3 +10,13 @@ int add(int a, int b){
 int multiply(int a, int b){
   return a*b;
 }
+
+int calculate(int a, int b, char c){
+  switch (c) {
+    case '*':
+      return multiply(a,b);
+    case '+':
+      return add(a,b);
+  }
+  return 0;
+}
--- Solver/parser.cpp
+++ Solver/parser.cpp
@@ -15,7 +15,9 @@ std::vector<Variable> _variables;
 
 Variable getVar(std::vector<Variable> variables, string varName){
   for(Variable v : variables){
-
+    if(v._varName == varName){
+      return v;
+    }
   }
 }
 
@@ -24,13 +26,31 @@ static bool readVariable(string ligne){
 
   string varName;
   string varValue;
-  if(variableType == "var"){
+  if(variableType == "var"){ //type variable (int)
     varName = ligne.substr(ligne.find(" ")+1, ligne.find("=")-ligne.find(" ")-1);
     varValue = ligne.substr(ligne.find("=")+1, ligne.find(";")-ligne.find("=")-1);
-    int f = stoi(varValue);
+
+    int f; // valeur a affecter
+    if(varValue.find("*") != string::npos){ //cas calcul sur des variable
+      Variable v1 = getVar(_variables, varValue.substr(0, varValue.find("*")));
+      int a = static_cast<const IntValue&>(*v1._value)._value;
+      Variable v2 = getVar(_variables, varValue.substr(varValue.find("*")+1,
+      varValue.length() - varValue.find("*")));
+      int b = static_cast<const IntValue&>(*v2._value)._value;
+
+      f = calculate(a,b,'*');
+    }
+    else if(!(isdigit(varValue[0]))){//cas d'affectation avec une autre variable
+      Variable v = getVar(_variables, varValue);
+      auto a = static_cast<const IntValue&>(*v._value)._value;
+      f = a;
+    }
+    else { //cas d'une valeur int
+      f = stoi(varValue);
+    }
     _variables.push_back(Variable(1, varName, 1, f));
   } else if(variableType == "set"){
-
+    
   } else if(variableType == "array"){
 
   }
